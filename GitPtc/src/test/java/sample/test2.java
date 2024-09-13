53a49737fbdaa877bb889c15a77a561e6cc8cadf
sanjaygit1 <rathinreddy14@gmail.com>
2024-09-13 19:44:11 +0530

    2

--- GitPtc/src/test/java/sample/test2.java
+++ GitPtc/src/test/java/sample/test2.java
@@ -4,6 +4,9 @@ public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.println("welcome");
 System.out.println("bye");
 
 System.out.println("thank you");
